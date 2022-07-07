231ca76ba9b725ea7d113356a5d849151c4439e4
viettu2k <doanviettu2k@gmail.com>
2022-07-07 17:41:16 +0700

    adding body part component

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,44 @@
 import React from 'react';
+import { Stack, Typography } from '@mui/material';
 
-const BodyPart = () => {
-  return <div>BodyPart</div>;
+import Icon from '../assets/icons/gym.png';
+
+const BodyPart = ({ item, bodyPart, setBodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={{
+        border: bodyPart === item ? '4px solid #ff2625' : '',
+        backgroundColor: '#fff',
+        borderBottomLeftRadius: '20px',
+        width: '270px',
+        height: '280px',
+        cursor: 'pointer',
+        gap: '47px',
+      }}
+      onClick={() => setBodyPart(item)}
+    >
+      <img
+        src={Icon}
+        alt="dumbbell"
+        style={{
+          width: '40px',
+          height: '40px',
+        }}
+      />
+      <Typography
+        fontSize="24px"
+        fontWeight="bold"
+        color="#3A1212"
+        textTransform="capitalize"
+      >
+        {item}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default BodyPart;
