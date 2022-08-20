48b59d21c7b779fb0f99a1888cc1ec952bb50f79
viettu2k <doanviettu2k@gmail.com>
2022-08-20 20:36:34 +0700

    fetch videos exercise details

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,13 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import { exerciseOptions, fetchData } from '../utils/fetchData';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,11 +17,18 @@ const ExerciseDetail = () => {
       const exercisesDbUrl = 'https://exercisedb.p.rapidapi.com';
       const youtubeSearchUrl =
         'https://youtube-search-and-download.p.rapidapi.com';
+
       const exerciseDetailData = await fetchData(
         `${exercisesDbUrl}/exercises/exercise/${id}`,
         exerciseOptions
       );
       setExerciseDetail(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?q=${exerciseDetail.name}`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData);
     };
 
     fetchExercisesData();
@@ -29,7 +37,10 @@ const ExerciseDetail = () => {
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
       <SimilarExercises />
     </Box>
   );
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,20 @@
+const EXERCISE_HOST = 'exercisedb.p.rapidapi.com';
+const YOUTUBE_HOST = 'youtube-search-and-download.p.rapidapi.com';
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
-    'X-RapidAPI-Host': process.env.REACT_APP_API_HOST,
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': EXERCISE_HOST,
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': YOUTUBE_HOST,
   },
 };
 
