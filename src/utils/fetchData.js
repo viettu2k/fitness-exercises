972741eb55a4744117aa5d232f5ff956a1609c32
viettu2k <doanviettu2k@gmail.com>
2022-09-04 21:46:14 +0700

    refactoring

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,26 +1,25 @@
-const EXERCISE_HOST = 'exercisedb.p.rapidapi.com';
-const YOUTUBE_HOST = 'youtube-search-and-download.p.rapidapi.com';
+const EXERCISE_HOST = "exercisedb.p.rapidapi.com";
+const YOUTUBE_HOST = "youtube-search-and-download.p.rapidapi.com";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 export const exerciseOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': EXERCISE_HOST,
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": EXERCISE_HOST,
   },
 };
 
 export const youtubeOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'X-RapidAPI-Key': API_KEY,
-    'X-RapidAPI-Host': YOUTUBE_HOST,
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": YOUTUBE_HOST,
   },
 };
 
 export const fetchData = async (url, options) => {
   const response = await fetch(url, options);
   const data = await response.json();
-
   return data;
 };
